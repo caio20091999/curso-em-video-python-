bfd3b8d10c61d54e3e900f4b834e6f277dcfa46e
Caio2009 <caioandrade0920@gmail.com>
2024-09-03 21:29:17 -0300

    att

---
+++ exercicios-python/ex003.py
@@ -0,0 +1,9 @@
+opcao = int(input("informe uma opção: [1] sacar \n[2] Extrato: "))
+
+if opcao == 1:
+    valor = float(input("Informe a quantia para o saque: "))
+    # ...
+elif opcao == 2:
+    print("exibndo o extrato...")
+else:
+    sys.exit("Opção inválida")
